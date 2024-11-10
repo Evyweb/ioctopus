4ef1f51a3bfebb20901f62ef390e5f9ac6c63020
Evyweb <dev@evyweb.fr>
2024-11-10 07:30:43 +0100

    refactor: add scope type

--- src/types.ts
+++ src/types.ts
@@ -4,20 +4,22 @@ export interface DependencyObject {
 
 export type DependencyArray = symbol[];
 
+export type Scope = 'singleton' | 'transient' | 'scoped';
+
 export interface Container {
     bind(key: symbol): {
-        toValue: (value: unknown, scope?: 'singleton' | 'transient' | 'scoped') => void;
-        toFunction: (fn: CallableFunction, scope?: 'singleton' | 'transient' | 'scoped') => void;
+        toValue: (value: unknown, scope?: Scope) => void;
+        toFunction: (fn: CallableFunction, scope?: Scope) => void;
         toHigherOrderFunction: (
             fn: CallableFunction,
             dependencies?: DependencyArray | DependencyObject,
-            scope?: 'singleton' | 'transient' | 'scoped'
+            scope?: Scope
         ) => void;
-        toFactory: (factory: CallableFunction, scope?: 'singleton' | 'transient' | 'scoped') => void;
+        toFactory: (factory: CallableFunction, scope?: Scope) => void;
         toClass: <C>(
             constructor: new (...args: any[]) => C,
             dependencies?: DependencyArray,
-            scope?: 'singleton' | 'transient' | 'scoped'
+            scope?: Scope
         ) => void;
     };
 
@@ -32,18 +34,18 @@ export interface Container {
 
 export interface Module {
     bind(key: symbol): {
-        toValue: (value: unknown, scope?: 'singleton' | 'transient' | 'scoped') => void;
-        toFunction: (fn: CallableFunction, scope?: 'singleton' | 'transient' | 'scoped') => void;
+        toValue: (value: unknown, scope?: Scope) => void;
+        toFunction: (fn: CallableFunction, scope?: Scope) => void;
         toHigherOrderFunction: (
             fn: CallableFunction,
             dependencies?: DependencyArray | DependencyObject,
-            scope?: 'singleton' | 'transient' | 'scoped'
+            scope?: Scope
         ) => void;
-        toFactory: (factory: CallableFunction, scope?: 'singleton' | 'transient' | 'scoped') => void;
+        toFactory: (factory: CallableFunction, scope?: Scope) => void;
         toClass: <C>(
             constructor: new (...args: any[]) => C,
             dependencies?: DependencyArray,
-            scope?: 'singleton' | 'transient' | 'scoped'
+            scope?: Scope
         ) => void;
     };
 
@@ -58,5 +60,5 @@ export type ResolveFunction = (dep: symbol) => unknown;
 
 export interface Binding {
     factory: (resolve: (key: symbol) => unknown) => unknown;
-    scope: 'singleton' | 'transient' | 'scoped';
+    scope: Scope;
 }
